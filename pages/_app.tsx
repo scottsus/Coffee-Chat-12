073a1ea5e3995fc5ecdab6338967b82d8fcb06f0
Scott Susanto <susantoscott@gmail.com>
2023-08-01 06:04:16 -0700

    Add metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,12 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const favicon =
-    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚀</text></svg>";
-
   const meta = {
-    title: '',
-    description: '',
-    image: favicon,
-    type: 'website',
+    title: 'Bean Brief',
+    description: 'Automated coffee chat assistant ☕️',
+    icons: {
+      icon: '/favicon.ico',
+    },
   };
 
   return (
@@ -20,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
-        <link rel="icon" href={favicon} />
+        <link rel="icon" href="/favicon.png" />
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
